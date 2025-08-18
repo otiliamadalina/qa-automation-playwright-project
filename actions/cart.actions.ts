cbdb60dab9cf17b194f463374ca0fff6e0f238ca
otiliamadalina <otilia-madalina.leporda@isa.utm.md>
2025-08-18 22:17:14 +0300

    fixed issue with sending report to telegram

--- actions/cart.actions.ts
+++ actions/cart.actions.ts
@@ -12,8 +12,6 @@ export default class CartActions extends BaseActions {
     this.cart = new CartPage(page, context);
   }
 
-
-
   async verifyCartProducts() {
     const items = this.cart.cartItems;
     const cartCount = await items.count();
@@ -48,9 +46,8 @@ export default class CartActions extends BaseActions {
     await this.cart.continueShoppingButton.click();
   }
 
-    async goToCheckout() {
+  async goToCheckout() {
     await expect(this.cart.checkoutButton).toBeVisible();
     await this.cart.checkoutButton.click();
   }
-
 }
--- pages/login.page.ts
+++ pages/login.page.ts
@@ -1,31 +1,27 @@
 import BasePage from "./base.page";
 
-export default class LoginPage extends BasePage{
-    get usernameInput(){
-        return this.page.getByRole("textbox", { name: "Username" });
-    }
+export default class LoginPage extends BasePage {
+  get usernameInput() {
+    return this.page.getByRole("textbox", { name: "Username" });
+  }
 
-    get passwordInput(){
-        return this.page.getByRole("textbox", { name: "Password" });
-    }
+  get passwordInput() {
+    return this.page.getByRole("textbox", { name: "Password" });
+  }
 
-    get loginButton(){
-        return this.page.getByRole("button", { name: "Login" });
-    }
+  get loginButton() {
+    return this.page.getByRole("button", { name: "Login" });
+  }
 
-    get getError(){
-        return this.page.locator("[data-test='error']");
-    }
+  get getError() {
+    return this.page.locator("[data-test='error']");
+  }
 
-    get logo(){
-        return this.page.locator("text=Swag Labs");
-    }   
+  get logo() {
+    return this.page.locator("text=Swag Labs");
+  }
 
-    h4Locator(usernameTitle: string) {
-        return this.page.locator("h4", {
-      hasText: `${usernameTitle}`,
-    });
-    }
+  h4Locator(usernameTitle: string) {
+    return this.page.locator("h4", { hasText: `${usernameTitle}` });
+  }
 }
-
-
